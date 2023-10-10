597b680cb9d86aa16303d8ec47ec057ec5be07d9
Joko109dogbn <105368445+Joko109dogbn@users.noreply.github.com>
2023-10-10 11:13:13 -0300

    okqqqqq

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom;
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_Clap').onclick = tocaSomClap;
+document.querySelector('.tecla_clap').onclick = tocaSomClap;

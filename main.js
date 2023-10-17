4a0950fb65994e54e8841226fa1ff24a92c60e76
Joko109dogbn <105368445+Joko109dogbn@users.noreply.github.com>
2023-10-17 10:58:42 -0300

    ok

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+//Criando/declarando a função tocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
+//chamando/invocando a função tocaSomPom pelo clique o botão pom
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap(){

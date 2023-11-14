f64ab081eedeffdae84001159f09d7a26515bfbc
Joko109dogbn <105368445+Joko109dogbn@users.noreply.github.com>
2023-11-14 11:16:47 -0300

    ok

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 //Criando/declarando a função tocaSomPom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(){
+    document.querySelector(IDaudio).play();
 }
 //chamando/invocando a função tocaSomPom pelo clique o botão pom
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
+//estrutura de repetição while = enquanto
+//mudar "while" para "for"
+    for(let contador = 0; contador < listaDeTeclas.length; contador++){
+       const tecla = listaDeTeclas[contador];
+       const efeito = tecla.classList[1]; //acessa o 2° item da classe de teclas
+       const IDaudio = `#som_${efeito}` ; //template string
+       tecla.onclick = function(){ //cria função anônima para chamar tocaSom
+               tocaSom(IDaudio);
+          }
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+        }
 }
-document.querySelector('.tecla_clap').onclick = tocaSomClap;

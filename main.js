e5f1a3778d0826cd91bcad992c47c62b85c409b2
Joko109dogbn <105368445+Joko109dogbn@users.noreply.github.com>
2023-11-28 19:04:18 -0300

    OK

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-//Criando/declarando a função tocaSomPom
-function tocaSom(){
-    document.querySelector(IDaudio).play();
-}
-//chamando/invocando a função tocaSomPom pelo clique o botão pom
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//estrutura de repetição while = enquanto
-//mudar "while" para "for"
-    for(let contador = 0; contador < listaDeTeclas.length; contador++){
-       const tecla = listaDeTeclas[contador];
-       const efeito = tecla.classList[1]; //acessa o 2° item da classe de teclas
-       const IDaudio = `#som_${efeito}` ; //template string
-       tecla.onclick = function(){ //cria função anônima para chamar tocaSom
-               tocaSom(IDaudio);
-          }
-    tecla.onkeydown = function(evento){
-        if(evento.code!= 'Tab'){
-              tecla.classList.add('ativa');
-        }
+//Criando/declarando a função tocasom com parâmetro idElementoAudio 
+function tocaSom (idElementoAudio){
+     //1 else elemento operadores comparação 
+         const elemento = document.querySelector(idElementoAudio); 
+         if(elemento && elemento.localName === 'audio') { 
+            elemento.play(); 
+         }else{ 
+         console.log("Elemento não encontrado"); 
+         }
+} 
+//criando referência constante listaDeTeclas buscando //todos os itens pela classe. tecla 
+const listaßeTeclas = document.querySelectorAll('.tecla'); 
+//estrutura de repetição "while significa  
+// enquanto algo acontecer mudou while para "for" 
+for{let contador = 0; contador listaDeTeclas.length;contador++) { 
+    const tecla listaDeTeclas [contador]; 
+    const efeito tecla.classList[1]; //acessa o 2º item da classe de teclas 
+    const idAudio #som $(efeito); //template string 
+    tecla.onclick = function(){ //cria função anónima para chamar tocaSom ]
+        tocaSom(idAudio);
     }
-        tecla.onkeyup = function(){
-          tecla.classList.remove('ativa');  
+        tecla.onkeydown = function(evento) { 
+            if(evento.code === 'Space' || evento.code === 'Enter'){   
+            tecla.classList.add('ativa"); 
+            }
         }
-        
-}
+            tecla. onkeyup=function(){ 
+                tecla.classList.remove('ativa');
+            }
+    }
